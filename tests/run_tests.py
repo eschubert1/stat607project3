db0477da9ec3c2047ffc7b55138e653870207d6d
eschubert1 <eschustring@gmail.com>
2025-10-20 16:12:13 -0400

    Adding tests of simulation functions

---
+++ tests/run_tests.py
@@ -0,0 +1,61 @@
+import pytest
+import numpy as np
+
+from ..src.dgps import generate_data
+from ..src.methods import compute_estimates, mle, empirical_bayes, logistic_irls
+from ..src.metrics import compute_metrics
+
+def test_estimation_size():
+    """
+        Test that estimates have appropriate dimensions
+    """
+    y, X, beta = generate_data(40, 10, 0.2, 0.2, 0.5, 1)
+    ests, vars, tau = compute_estimates(y, X, 0.25)
+
+    n = np.shape(X)[1]
+    assert np.shape(ests) == (6, n)
+    assert np.shape(vars) == (6, n)
+
+def test_estimation_range():
+    """
+        Test that estimates are in expected ranges
+    """
+    y, X, beta = generate_data(40, 10, 0.2, 0.2, 0.5, 1)
+    ests, vars, tau = compute_estimates(y, X, 0.25)
+
+    for arr in vars:
+        for element in arr:
+            if element is not None:
+                if element < 0:
+                    raise ValueError("Variance estimates must be non-negative")
+    
+    if tau < 0:
+        raise ValueError("Estimate of prior variance must be non-negative")
+    
+def test_empirical_bayes():
+    """
+        Test that empirical bayes function returns reasonable estimates when
+        MLE returns reasonable estimates
+    """
+    ratios = []
+    for i in range(1000):
+        y, X, beta = generate_data(40, 10, 0.2, 0.2, 0.5, 1)
+        n = np.shape(X)[1]
+        ml_beta, ml_cov = mle(y,X)
+        if np.array_equal(ml_cov, np.zeros((n,n))):
+            continue
+        if np.any(np.diag(ml_cov) > 100):
+            continue
+        eb_beta, eb_cov, tau2 = empirical_bayes(y, X, ml_beta, ml_cov)
+        ratios.append(eb_beta/ml_beta)
+    
+    ratios = np.array(ratios)
+    if ratios.max() > 10:
+        raise ValueError("Empirical Bayes estimates diverging")
+    
+def test_rng_consistency():
+    y, X, beta = generate_data(100, 4, 0.2, 0.2, 0.5, 1, rng=np.random.default_rng(32))
+    ynew, Xnew, betanew = generate_data(100, 4, 0.2, 0.2, 0.5, 1, rng=np.random.default_rng(32))
+    assert np.array_equal(y, ynew)
+    assert np.array_equal(X, Xnew)
+    assert np.array_equal(beta, betanew)
